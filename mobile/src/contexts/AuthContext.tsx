e8ed111d6f512a86c4226b9f14cd1e1937e8af67
TecJP <tec.joao.freitas@gmail.com>
2022-11-03 10:41:11 -0300

    Finished the components creation and finished newPool and findPool pages in mobile.

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,67 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
+
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  isUserLoggingIn: boolean;
+  signIn: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [isUserLoggingIn, setIsUserLoggingIn] = useState(false);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '64463400029-kfj4pbb1l7tlg384u2cu2sjksvmjogbc.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  });
+
+
+  async function signIn() {
+    try {
+      setIsUserLoggingIn(true);
+      await promptAsync();
+
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoggingIn(false);
+    }
+  }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log(access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
+  return (
+    <AuthContext.Provider value={{
+      signIn,
+      isUserLoggingIn,
+      user,
+    }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
